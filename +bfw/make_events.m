9f70728d0448eea69b6ed2e541d039f2e1d739de
nfagan <nick@fagan.org>
2018-01-23 18:55:24 -0500

    fix events

--- +bfw/make_events.m
+++ +bfw/make_events.m
@@ -1,7 +1,11 @@
 function make_events( duration )
 
+import shared_utils.logical.find_starts;
+
 bounds_p = bfw.get_intermediate_directory( 'bounds' );
-unified_p = bfw.get_intermediate_directory( 'unified' );
+save_p = bfw.get_intermediate_directory( 'events' );
+
+shared_utils.io.require_dir( save_p );
 
 bound_mats = shared_utils.io.find( bounds_p, '.mat' );
 
@@ -12,12 +16,20 @@ for i = 1:numel(bound_mats)
   m1 = bounds.m1.bounds;
   m2 = bounds.m2.bounds;
   
-  meta = shared_utils.io.fload( fullfile(unified_p, bounds.m1.unified_filename) );
+  unified_filename = bounds.m1.unified_filename;
   
   m1t = bounds.m1.time;
   m2t = bounds.m2.time;
   
   roi_names = m1.keys();
+  
+  all_events = cell( numel(roi_names), 3 );
+  event_roi_key = containers.Map();
+  monk_key = containers.Map();
+  
+  monk_key('m1') = 1;
+  monk_key('m2') = 2;
+  monk_key('mutual') = 3;
   
   for j = 1:numel(roi_names)
     
@@ -27,12 +39,27 @@ for i = 1:numel(bound_mats)
     m2_bounds = m2(roi_name);
     mutual_bounds = m1_bounds & m2_bounds;
 
-    m1_evts = shared_utils.logical.find_starts( m1_bounds, evt_length );
-    m2_evts = shared_utils.logical.find_starts( m2_bounds, evt_length );
-    mutual = shared_utils.logical.find_starts( mutual_bounds, evt_length );
+    m1_evts = find_starts( m1_bounds, duration );
+    m2_evts = find_starts( m2_bounds, duration );
+    mutual = find_starts( mutual_bounds, duration );
     
-    d = 10;
+    m1_evts = arrayfun( @(x) m1t(x), m1_evts );
+    m2_evts = arrayfun( @(x) m2t(x), m2_evts );
+    mutual = arrayfun( @(x) m1t(x), mutual );
+    
+    all_events(j, :) = { m1_evts, m2_evts, mutual };
+    
+    event_roi_key(roi_name) = j;
   end
+  
+  events = struct();
+  
+  events.times = all_events;
+  events.roi_key = event_roi_key;
+  events.monk_key = monk_key;
+  events.unified_filename = unified_filename;
+  
+  save( fullfile(save_p, unified_filename), 'events' );
 end
 
 end
