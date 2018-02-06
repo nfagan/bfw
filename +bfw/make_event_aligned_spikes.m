41d3394507383f73de2ec3bdf836c2c188babc3f
nfagan <nick@fagan.org>
2018-02-06 16:08:18 -0500

    reconvert to parallel

--- +bfw/make_event_aligned_spikes.m
+++ +bfw/make_event_aligned_spikes.m
@@ -37,7 +37,7 @@ null_n_iterations = params.null_n_iterations;
 
 allow_overwrite = params.overwrite;
 
-for i = 1:numel(event_files)
+parfor i = 1:numel(event_files)
   fprintf( '\n %d of %d', i, numel(event_files) );
   
   events = fload( event_files{i} );
