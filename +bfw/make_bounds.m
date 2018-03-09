1bdff48c92464d9114ca9bf020f3f9ce2fb67aec
nfagan <nick@fagan.org>
2018-03-09 11:30:54 -0500

    change fixation criteria for bounds

--- +bfw/make_bounds.m
+++ +bfw/make_bounds.m
@@ -31,7 +31,7 @@ copy_fields = { 'unified_filename', 'aligned_filename', 'aligned_directory' };
 window_size = params.window_size;
 step_size = params.step_size;
 
-parfor i = 1:numel(aligned_mats)
+for i = 1:numel(aligned_mats)
   fprintf( '\n %d of %d', i, numel(aligned_mats) );
   
   aligned = shared_utils.io.fload( aligned_mats{i} );
@@ -70,6 +70,7 @@ parfor i = 1:numel(aligned_mats)
   
   if ( bfw.conditional_skip_file(full_filename, params.overwrite) ), continue; end
   
+  %   first get bounds  
   for k = 1:numel(fields)
     bounds.(fields{k}).bounds = containers.Map();
     for j = 1:numel(rect_keys)
@@ -100,21 +101,7 @@ parfor i = 1:numel(aligned_mats)
         n_included_blink_x = NaN;
         n_included_blink_y = NaN;
         n_included_samples = NaN;
-      end
-      
-      %   & operation to include fixations, only
-      if ( params.require_fixation )
-        m_ib = m_ib & fix_file.(fields{k}).is_fixation;
-      end
-      
-      %   add sliding window
-      if ( params.update_time )
-        [m_ib, t] = slide_window( m_ib, t, window_size, step_size );
-      else
-        m_ib = slide_window( m_ib, t, window_size, step_size );
-      end
-      %   end sliding window
-      
+      end      
       bounds.(fields{k}).bounds(key) = m_ib;
       bounds.(fields{k}).time = t;
       bounds.(fields{k}).n_included_blinks = n_included_blinks;
@@ -128,6 +115,76 @@ parfor i = 1:numel(aligned_mats)
     bounds.(fields{k}).bounds_filename = b_filename;
     bounds.(fields{k}).bounds_directory = save_p;
   end
+  
+  %   then apply fixations
+  for k = 1:numel(fields)
+    c_fix = fix_file.(fields{k});
+    c_bounds = bounds.(fields{k}).bounds;
+    
+    t = bounds.(fields{k}).time;
+    
+    fix_id = cell( 1, numel(c_fix.start_indices) );
+            
+    for j = 1:numel(c_fix.start_indices)
+      start_index = c_fix.start_indices(j);
+      stop_index = c_fix.stop_indices(j);
+      
+      ns = zeros( size(rect_keys) );
+      
+      for h = 1:numel(rect_keys)
+        roi = rect_keys{h};
+        c_bounds_vec = c_bounds(roi);        
+        ns(h) = sum( c_bounds_vec(start_index:stop_index) );
+      end
+      
+      %   right now, we assign a fixation to an roi based on the maximum
+      %   number of samples that fall into that roi.
+      [~, max_index] = max( ns );
+      
+      fix_id{j} = rect_keys{max_index};
+    end
+    
+    per_roi_fix = containers.Map();
+    
+    for j = 1:numel(rect_keys)
+      roi = rect_keys{j};
+      
+      adjusted_fix_vec = false( size(c_fix.is_fixation) );
+      
+      matching_fix = find( strcmp(fix_id, roi) );
+      
+      for h = 1:numel(matching_fix)
+        fix_index = matching_fix(h);
+        start_index = c_fix.start_indices(fix_index);
+        stop_index = c_fix.stop_indices(fix_index);
+        adjusted_fix_vec(start_index:stop_index) = true;
+      end
+      
+      per_roi_fix(roi) = adjusted_fix_vec;      
+    end
+    
+    for j = 1:numel(rect_keys)
+      roi = rect_keys{j};
+      
+      m_ib = c_bounds(roi);
+      
+      %   & operation to include fixations, only
+      if ( params.require_fixation )
+        m_ib = m_ib & per_roi_fix(roi);
+      end
+      
+      %   add sliding window
+      if ( params.update_time )
+        [m_ib, t] = slide_window( m_ib, t, window_size, step_size );
+      else
+        m_ib = slide_window( m_ib, t, window_size, step_size );
+      end
+      %   end sliding window
+      
+      bounds.(fields{k}).bounds(key) = m_ib;
+      bounds.(fields{k}).time = t;
+    end
+  end  
   
   bounds.window_size = window_size;
   bounds.step_size = step_size;
@@ -168,3 +225,67 @@ while ( stop <= N )
 end
 
 end
+
+% %
+%   % old bounds
+%   %
+%   
+%   
+%   for k = 1:numel(fields)
+%     bounds.(fields{k}).bounds = containers.Map();
+%     for j = 1:numel(rect_keys)
+%       key = rect_keys{j};
+%       
+%       x = aligned.(fields{k}).position(1, :);
+%       y = aligned.(fields{k}).position(2, :);
+%       t = aligned.(fields{k}).time;
+%       
+%       m_rect = rect.(fields{k}).rects(key);
+%       
+%       m_blink_thresh = NaN;
+%       
+%       if ( params.remove_blink_nans )
+%         m_blink_thresh = round( median(blinks.(fields{k}).durations) );
+%       end
+%       
+%       %   if there are no blinks ... (unlikely)
+%       if ( isnan(m_blink_thresh) ), m_blink_thresh = Inf; end
+%       
+%       if ( params.remove_blink_nans )
+%         [m_ib, n_included_blink_x, n_included_blink_y, n_included_samples] = ...
+%           bfw.bounds.rect_excluding_blinks( x, y, m_rect, m_blink_thresh );
+%         n_included_blinks = max( n_included_blink_x, n_included_blink_y );
+%       else
+%         m_ib = bfw.bounds.rect( x, y, m_rect );
+%         n_included_blinks = NaN;
+%         n_included_blink_x = NaN;
+%         n_included_blink_y = NaN;
+%         n_included_samples = NaN;
+%       end
+%       
+%       %   & operation to include fixations, only
+%       if ( params.require_fixation )
+%         m_ib = m_ib & fix_file.(fields{k}).is_fixation;
+%       end
+%       
+%       %   add sliding window
+%       if ( params.update_time )
+%         [m_ib, t] = slide_window( m_ib, t, window_size, step_size );
+%       else
+%         m_ib = slide_window( m_ib, t, window_size, step_size );
+%       end
+%       %   end sliding window
+%       
+%       bounds.(fields{k}).bounds(key) = m_ib;
+%       bounds.(fields{k}).time = t;
+%       bounds.(fields{k}).n_included_blinks = n_included_blinks;
+%       bounds.(fields{k}).n_included_blink_x = n_included_blink_x;
+%       bounds.(fields{k}).n_included_blink_y = n_included_blink_y;
+%       bounds.(fields{k}).n_included_samples = n_included_samples;
+%     end
+%     for j = 1:numel(copy_fields)
+%       bounds.(fields{k}).(copy_fields{j}) = aligned.(fields{k}).(copy_fields{j});
+%     end
+%     bounds.(fields{k}).bounds_filename = b_filename;
+%     bounds.(fields{k}).bounds_directory = save_p;
+%   end
