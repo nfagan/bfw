7ab520a7bec05522f32b54116616e0873d42ad14
nfagan <nick@fagan.org>
2018-08-01 16:50:50 -0400

    showcase eyes vs. face

--- script/bfw_plot_mua.m
+++ script/bfw_plot_mua.m
@@ -1,3 +1,5 @@
+function bfw_plot_mua()
+
 per_trial_p = bfw.get_intermediate_directory( 'per_trial_mua' );
 mats = bfw.require_intermediate_mats( per_trial_p );
 
@@ -6,6 +8,50 @@ min_isi = 0;
 [dat, labs, t, params] = bfw.get_mua_psth( mats ...
   , 'min_interspike_interval', min_isi ...
 );
+
+%%  count n trials
+
+countlabs = labs';
+
+[daylabs, day_i] = keepeach( countlabs', 'session_name' );
+
+ns = zeros( size(day_i) );
+
+for i = 1:numel(day_i)
+  chans = combs( countlabs, 'channel', day_i{i} );
+  
+  mask = fcat.mask( countlabs, day_i{i}, @find, {chans{1}, 'm1'} );
+  
+  ns(i) = count( countlabs, 'eyes', mask ); 
+end
+
+[ daylabs(:, 'session_name'), arrayfun(@(x) {x}, ns) ]
+
+%%  face vs. outside
+
+pltlabs = labs';
+pltdat = dat;
+
+gcats = { 'looks_to' };
+pcats = { 'region', 'looks_by' };
+
+% units = findall( pltlabs, {'channel', 'session_name'}, find(pltlabs, 'bla') );
+
+mask = fcat.mask( pltlabs ...
+  , @findor,    {'outside1', 'face'} ...
+  , @find,      { 'm1', 'bla' } ...
+  , @findnone,  {'eyes', 'mouth', 'ref'} ...
+);
+
+pl = plotlabeled.make_common( 'sort_combinations', true, 'x', t );
+pl.fig = figure(2);
+
+axs = pl.lines( rowref(pltdat, mask), pltlabs(mask), gcats, pcats );
+
+shared_utils.plot.hold( axs );
+shared_utils.plot.add_vertical_lines( axs, 0 );
+
+
 
 %%
 
