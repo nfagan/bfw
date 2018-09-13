75d64c52e51f0843374812880b3c1534bd33c955
nfagan <nick@fagan.org>
2018-09-13 13:33:50 -0400

    hist of inter-stim distances

--- script/plot_inter_stim_interval.m
+++ script/plot_inter_stim_interval.m
@@ -14,6 +14,11 @@ for i = 1:numel(stim_files)
   stim_file = shared_utils.io.fload( stim_files{i} );
   un_file = shared_utils.io.fload( fullfile(unified_p, stim_file.unified_filename) );
   
+  monk_dirs = fieldnames( un_file );
+  first_field = monk_dirs{1};
+  
+  task_type = bfw.field_or( un_file.(first_field), 'task_type', 'free_viewing' );
+  
   stim_isis = diff( stim_file.stimulation_times );
   sham_isis = diff( stim_file.sham_times );
   
@@ -21,6 +26,7 @@ for i = 1:numel(stim_files)
       'unified_filename', stim_file.unified_filename ...
     , 'session', un_file.m1.mat_directory_name ...
     , 'stim_type', 'stim' ...
+    , 'task_type', task_type ...
   );
 
   n_stim = numel( stim_isis );
@@ -34,15 +40,41 @@ end
 
 %%  plot
 
-pl = plotlabeled();
+f = figure(1);
+clf( f );
 
 pltdat = all_isis;
 pltlabs = all_labs';
 
-pcats = { 'stim_type' };
+pcats = { 'stim_type', 'task_type' };
+[I, C] = findall( pltlabs, pcats );
+shp = plotlabeled.get_subplot_shape( numel(I) );
+axs = gobjects( size(I) );
 
-pl.hist( pltdat, pltlabs, pcats, 100 );
+meds = nan( size(I) );
+ns = nan( size(I) );
 
+for i = 1:numel(I)
+  ax = subplot( shp(1), shp(2), i );
+  
+  some_dat = pltdat(I{i});
+  hist( ax, some_dat, 100 );
+  hold( ax, 'on' );
+  
+  title( ax, strrep(strjoin(C(:, i), ' | '), '_', ' ') );
+  
+  meds(i) = median( some_dat );
+  ns(i) = numel( some_dat );
+  axs(i) = ax;
+end
 
+shared_utils.plot.match_xlims( axs );
+shared_utils.plot.match_ylims( axs );
+
+ys = get( axs(1), 'ylim' );
+
+arrayfun( @(x, y) plot(x, [y;y], ys, 'k--', 'linewidth', 2), axs, meds );
+arrayfun( @(x, y, z) text(x, y(1)+2, ys(2)-1, sprintf('M=%0.2f, N=%d', y, z)) ...
+  , axs, meds, ns );
 
 end
