8b98bcd0b56fe43f4d9bb9a5c728fd850cf4bd46
nfagan <nick@fagan.org>
2018-10-02 14:23:31 -0400

    change edf -> edfsync

--- script/debug_stim_times.m
+++ script/debug_stim_times.m
@@ -9,8 +9,9 @@ conf = params.config;
 
 unified_p = bfw.gid( 'unified', conf );
 stim_p = bfw.gid( 'stim', conf );
-sync_p = bfw.gid( 'sync', conf );
-edf_p = bfw.gid( 'edf', conf );
+mat_sync_p = bfw.gid( 'sync', conf );
+edf_samples_p = bfw.gid( 'edf_raw_samples', conf );
+edf_sync_p = bfw.gid( 'edf_sync', conf );
 roi_p = bfw.gid( 'rois', conf );
 
 look_ahead = 5;
@@ -34,8 +35,9 @@ parfor i = 1:numel(mats)
 
   try
     un_file = fload( fullfile(unified_p, unified_filename) );
-    sync_file = fload( fullfile(sync_p, unified_filename) );
-    edf_file = fload( fullfile(edf_p, unified_filename) );
+    mat_sync_file = fload( fullfile(mat_sync_p, unified_filename) );
+    edf_sync_file = fload( fullfile(edf_sync_p, unified_filename) );
+    edf_samples_file = fload( fullfile(edf_samples_p, unified_filename) );
     roi_file = fload( fullfile(roi_p, unified_filename) );
   catch err
     warning( err.message );
@@ -49,9 +51,9 @@ parfor i = 1:numel(mats)
   process_params.time = plot_t;
 
   try 
-    [ib, labs] = one_file( un_file, stim_file, sync_file, edf_file, roi_file, process_params );
+    [ib, labs] = one_file( un_file, stim_file, mat_sync_file, edf_sync_file, edf_samples_file, roi_file, process_params );
   catch err
-    warning( err.message );
+    warning( '"%s" (%d) failed with message: "%s"', unified_filename, i, err.message );
     is_ok(i) = false;
     continue;
   end
@@ -60,7 +62,7 @@ parfor i = 1:numel(mats)
     all_ib{i} = ib;
     all_labs{i} = labs;
   catch err
-    warning( err.message );
+    warning( '"%s" (%d) failed with message: "%s"', unified_filename, i, err.message );
     is_ok(i) = false;
     continue;
   end
@@ -74,61 +76,75 @@ ib = vertcat( all_ib{:} );
 
 end
 
-function [aligned_ib, labs] = one_file( un_file, stim_file, sync_file, edf_file, roi_file, params)
+function [all_aligned_ib, all_labs] = one_file(un_file, stim_file, mat_sync_file, edf_sync_file, edf_samples_file, roi_file, params)
 
 lb = params.look_back;
 la = params.look_ahead;
 t = params.time;
 
-m_ind = strcmp( sync_file.sync_key, 'mat' );
-p_ind = strcmp( sync_file.sync_key, 'plex' );
+m_ind = strcmp( mat_sync_file.sync_key, 'mat' );
+p_ind = strcmp( mat_sync_file.sync_key, 'plex' );
 
-edf = edf_file.m1.edf;
-messages = edf.Events.Messages;
-edf_time = edf.Samples.time;
+edf_time = edf_samples_file.m1.t;
+edf_x = edf_samples_file.m1.x;
+edf_y = edf_samples_file.m1.y;
 
-edf_sync_times = columnize( messages.time(strcmp(messages.info, 'RESYNCH')) );
-mat_sync_times = sync_file.plex_sync(2:end, m_ind);
-plex_sync_times = sync_file.plex_sync(2:end, p_ind);
+edf_sync_times = edf_sync_file.m1.edf_sync_times;
+mat_sync_times = mat_sync_file.plex_sync(2:end, m_ind);
+plex_sync_times = mat_sync_file.plex_sync(2:end, p_ind);
 
 assert( numel(edf_sync_times) == numel(mat_sync_times) && ...
   numel(mat_sync_times) == numel(plex_sync_times), 'Sync times do not match.' );
 
-m1_eyes_roi = roi_file.m1.rects('eyes_nf');
+roi_names = keys( roi_file.m1.rects );
+comb_indices = combvec( 1:numel(roi_names) );
 
-allow_oob = false;
-edf_plex_time = edf_to_plex_time( edf_time, edf_sync_times, plex_sync_times, allow_oob );
+all_labs = fcat();
+all_aligned_ib = [];
 
-ib = bfw.bounds.rect( edf.Samples.posX(:), edf.Samples.posY(:), m1_eyes_roi );
-
-sham_times = stim_file.sham_times(:);
-stim_times = stim_file.stimulation_times(:);
-
-stim_events = [ stim_times; sham_times ];
-stim_types = { 'stim', 'sham' };
-stim_type_indices = [ ones(numel(stim_times), 1); repmat(2, numel(sham_times), 1) ];
-
-aligned_ib = false( numel(stim_events), numel(t) );
-
-labs = fcat();
-
-for i = 1:numel(stim_events)
-  start_t = stim_events(i) + lb;
-  stop_t = stim_events(i) + la;
+for idx = 1:size(comb_indices, 2)
   
-  [~, start_ind] = min( abs(edf_plex_time - start_t) );
-  [~, stop_ind] = min( abs(edf_plex_time - stop_t) );
+  target_roi_name = roi_names{comb_indices(1, idx)};
+  target_roi = roi_file.m1.rects(target_roi_name);
+
+  allow_oob = false;
+  edf_plex_time = edf_to_plex_time( edf_time, edf_sync_times, plex_sync_times, allow_oob );
+
+  ib = bfw.bounds.rect( edf_x(:), edf_y(:), target_roi );
+
+  sham_times = stim_file.sham_times(:);
+  stim_times = stim_file.stimulation_times(:);
+
+  stim_events = [ stim_times; sham_times ];
+  stim_types = { 'stim', 'sham' };
+  stim_type_indices = [ ones(numel(stim_times), 1); repmat(2, numel(sham_times), 1) ];
+
+  aligned_ib = false( numel(stim_events), numel(t) );
+
+  labs = fcat();
+
+  for i = 1:numel(stim_events)
+    start_t = stim_events(i) + lb;
+    stop_t = stim_events(i) + la;
+
+    [~, start_ind] = min( abs(edf_plex_time - start_t) );
+    [~, stop_ind] = min( abs(edf_plex_time - stop_t) );
+
+    c_n = stop_ind - start_ind + 1;
+    use_n = min( c_n, numel(t) );
+
+    aligned_ib(i, 1:use_n) = ib(start_ind:start_ind+use_n-1);
+
+    append( labs, fcat.create(...
+        'unified_filename', stim_file.unified_filename ...
+      , 'session', un_file.m1.mat_directory_name ...
+      , 'stim_type', stim_types{stim_type_indices(i)} ...
+      , 'roi', target_roi_name ...
+    ));
+  end
   
-  c_n = stop_ind - start_ind + 1;
-  use_n = min( c_n, numel(t) );
-  
-  aligned_ib(i, 1:use_n) = ib(start_ind:start_ind+use_n-1);
-  
-  append( labs, fcat.create(...
-      'unified_filename', stim_file.unified_filename ...
-    , 'session', un_file.m1.mat_directory_name ...
-    , 'stim_type', stim_types{stim_type_indices(i)} ...
-  ));
+  all_aligned_ib = [ all_aligned_ib; aligned_ib ];
+  append( all_labs, labs );
 end
 
 end
@@ -152,9 +168,8 @@ for i = 1:numel(events_a)
     continue;
   end
   
-  % time is earlier than sync time
-  
   if ( t < sync_a )
+    % target-time is earlier than sync time
     if ( I == 1 )
       if ( allow_oob )
         t1a = nan;
@@ -170,6 +185,7 @@ for i = 1:numel(events_a)
     t2a = clock_a(I);
     t2b = clock_b(I);
   else
+    % target-time is after sync time
     t1a = clock_a(I);
     t1b = clock_b(I);
     
