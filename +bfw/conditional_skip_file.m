367c2cb7ef44f584b4beca31212dcc839ddca806
nfagan <nick@fagan.org>
2018-10-11 17:14:40 -0400

    make name skipped

--- +bfw/conditional_skip_file.m
+++ +bfw/conditional_skip_file.m
@@ -3,7 +3,8 @@ function should_skip = conditional_skip_file( file, allow_overwrite )
 should_skip = false;
 
 if ( ~allow_overwrite && shared_utils.io.fexists(file) )
-  fprintf( '\n Skipping "%s" because it already exists.', file );
+  [~, name] = fileparts( file );
+  fprintf( '\n Skipping "%s" because it already exists.', name );
   should_skip = true;
 end
 
