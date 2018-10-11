4f4d42f3517b118528884fe783053f045f35cf17
nfagan <nick@fagan.org>
2018-10-11 17:07:20 -0400

    add pl2 file info

--- +bfw/make_plex_meta.m
+++ +bfw/make_plex_meta.m
@@ -9,6 +9,7 @@ isd = params.input_subdir;
 osd = params.output_subdir;
 
 meta_p = bfw.gid( fullfile('meta', isd), conf );
+un_p = bfw.gid( fullfile('unified', isd), conf );
 plex_meta_p = bfw.gid( fullfile('plex_meta', osd), conf );
 
 meta_map = bfw.get_plex_meta_channel_map( conf );
@@ -19,13 +20,20 @@ regions = meta_map('region');
 
 mats = bfw.require_intermediate_mats( params.files, meta_p, params.files_containing );
 
-parfor i = 1:numel(mats)
+for i = 1:numel(mats)
   shared_utils.general.progress( i, numel(mats), mfilename );
   
   meta_file = shared_utils.io.fload( mats{i} );
   
-  unified_filename = meta_file.unified_filename;
-  output_filename = fullfile( plex_meta_p, unified_filename );
+  un_filename = meta_file.unified_filename;
+  output_filename = fullfile( plex_meta_p, un_filename );
+  
+  try
+    un_file = shared_utils.io.fload( fullfile(un_p, un_filename) );
+  catch err
+    bfw.print_fail_warn( un_filename, err.message );
+    continue;
+  end
   
   if ( bfw.conditional_skip_file(output_filename, params.overwrite) )
     continue;
@@ -36,24 +44,31 @@ parfor i = 1:numel(mats)
   session_index = strcmp( sessions, session );
   
   if ( nnz(session_index) == 0 )
-    bfw.print_fail_warn( unified_filename, 'No plex-meta data was defined.' );
+    bfw.print_fail_warn( un_filename, 'No plex-meta data was defined.' );
     continue;
   end
   
-  plex_meta_file = struct();
-  plex_meta_file.unified_filename = unified_filename;
-  plex_meta_file.params = params;
-  
-  chans = channels(session_index);
-  regs = regions(session_index);
-  
-  [regs, chans] = linearize_channels_regions( chans, regs );
-  
-  plex_meta_file.channels = chans;
-  plex_meta_file.regions = regs;
-  
-  shared_utils.io.require_dir( plex_meta_p );
-  shared_utils.io.psave( output_filename, plex_meta_file, 'plex_meta_file' );
+  try
+    plex_meta_file = struct();
+    plex_meta_file.unified_filename = un_filename;
+    plex_meta_file.params = params;
+    plex_meta_file.pl2_fileparts = cshorzcat( un_file.m1.plex_directory, un_file.m1.plex_filename );
+
+    chans = channels(session_index);
+    regs = regions(session_index);
+
+    [regs, chans] = linearize_channels_regions( chans, regs );
+
+    plex_meta_file.channels = chans;
+    plex_meta_file.regions = regs;
+
+    shared_utils.io.require_dir( plex_meta_p );
+    shared_utils.io.psave( output_filename, plex_meta_file, 'plex_meta_file' );
+    
+  catch err
+    bfw.print_fail_warn( un_filename, err.message );
+    continue;
+  end
 end
 
 end
