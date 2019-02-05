37689b5de38cf8fa91f91f89a0947a5e886ea2aa
nfagan <nick@fagan.org>
2019-02-05 15:09:08 -0500

    update build

--- mex/bfw_build_m1_m2_align.m
+++ mex/bfw_build_m1_m2_align.m
@@ -6,7 +6,13 @@ out_dir = fullfile( proj_dir, '+bfw', '+mex' );
 
 src_file = fullfile( mex_dir, 'm1_m2_align.cpp' );
 
-cmd = sprintf( 'mex -outdir "%s" "%s"', out_dir, src_file );
+if ( isunix() && ~ismac() )
+  compiler_spec = 'GCC=''/usr/bin/gcc-4.9'' G++=''/usr/bin/g++-4.9'' ';
+else
+  compiler_spec = '';
+end
+
+cmd = sprintf( 'mex -v %s -outdir "%s" "%s"', compiler_spec, out_dir, src_file );
 eval( cmd );
 
 end
