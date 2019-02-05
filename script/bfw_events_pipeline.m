cbfc36c95cd1c00b917aecfc2776c5abf8450cb7
nfagan <nick@fagan.org>
2019-02-05 17:31:16 -0500

    typo

--- script/bfw_events_pipeline.m
+++ script/bfw_events_pipeline.m
@@ -13,8 +13,8 @@ end
 
 handle_bounds( params, sessions );
 
-bfw.make_aligned_samples( inputs );
-bfw.make_binned_aligned_samples( inputs );
+bfw.make_aligned_samples( params );
+bfw.make_binned_aligned_samples( params );
 
 % bfw.make_raw_aligned_samples( params );
 % bfw.make_binned_raw_aligned_samples( params );
