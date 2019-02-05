43dc2204da8430cdcb8c5b263704ddee071df7e0
nfagan <nick@fagan.org>
2019-02-05 15:10:51 -0500

    update build

--- mex/bfw_build_m1_m2_align.m
+++ mex/bfw_build_m1_m2_align.m
@@ -8,11 +8,14 @@ src_file = fullfile( mex_dir, 'm1_m2_align.cpp' );
 
 if ( isunix() && ~ismac() )
   compiler_spec = 'GCC=''/usr/bin/gcc-4.9'' G++=''/usr/bin/g++-4.9'' ';
+  addtl_cxx_flags = 'CXXFLAGS="-std=c++1y -fPIC"';
 else
   compiler_spec = '';
+  addtl_cxx_flags = '';
 end
 
-cmd = sprintf( 'mex -v %s -outdir "%s" "%s"', compiler_spec, out_dir, src_file );
+cmd = sprintf( 'mex -v %s%s -outdir "%s" "%s"', compiler_spec ...
+  , addtl_cxx_flags, out_dir, src_file );
 eval( cmd );
 
 end
