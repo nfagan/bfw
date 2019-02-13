33d83793b5ce05d06e9f7e972b32ce7607570f69
nfagan <nick@fagan.org>
2019-02-13 17:42:13 -0500

    updated linearized events

--- script/bfw_example_linearized_events.m
+++ script/bfw_example_linearized_events.m
@@ -6,6 +6,8 @@ event_labels = linearized_events.labels';
 events = linearized_events.events;
 event_key = linearized_events.event_key;
 
+[mat_labels, mat_categories] = categorical( event_labels );
+
 start_index_col = event_key('start_index');  
 stop_index_col = event_key('stop_index');
 
@@ -19,3 +21,6 @@ I = findall( event_labels, {'unified_filename'}, mask );
 
 non_overlapping = ...
   bfw_exclusiveize_events( start_indices, stop_indices, event_labels, rois, I );
+
+non_overlapping_events = events(non_overlapping, :);
+non_overlapping_labels = mat_labels(non_overlapping, :);
