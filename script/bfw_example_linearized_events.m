a0b70294cc6c70cb1b30670f1245984555a946db
nfagan <nick@fagan.org>
2019-02-14 12:18:52 -0500

    add mutual specifier

--- script/bfw_example_linearized_events.m
+++ script/bfw_example_linearized_events.m
@@ -1,6 +1,8 @@
+%% Sort all events in ascending order by event time.
+
 linearized_events = bfw_linearize_events();
 
-%%
+%%  Ensure events are non-overlapping
 
 event_labels = linearized_events.labels';
 events = linearized_events.events;
@@ -15,7 +17,7 @@ start_indices = events(:, start_index_col);
 stop_indices = events(:, stop_index_col);
 
 rois = { 'eyes_nf', 'mouth', 'face' };
-mask = find( event_labels, {'m1', 'm2'} );
+mask = find( event_labels, {'m1', 'm2', 'mutual'} );
 
 I = findall( event_labels, {'unified_filename'}, mask );
 
@@ -24,3 +26,24 @@ non_overlapping = ...
 
 non_overlapping_events = events(non_overlapping, :);
 non_overlapping_labels = mat_labels(non_overlapping, :);
+
+%%  Matlab native
+
+roi_col = strcmp( mat_categories, 'roi' );
+looks_by_col = strcmp( mat_categories, 'looks_by' );
+
+is_eyes = mat_labels(non_overlapping, roi_col) == 'eyes_nf';
+is_m1 = mat_labels(non_overlapping, looks_by_col) == 'm1';
+is_m2 = mat_labels(non_overlapping, looks_by_col) == 'm2';
+
+is_m1eyes = is_m1 & is_eyes;
+
+%%  fcat
+
+is_m1eyes_fcat = find( event_labels, {'m1', 'eyes_nf', '01162018'}, non_overlapping );
+
+
+
+
+
+
