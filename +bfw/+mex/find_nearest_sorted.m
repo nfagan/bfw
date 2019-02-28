ea3fb41a866b6044c27fdcacb899a7752a41d962
nfagan <nick@fagan.org>
2019-02-28 17:32:04 -0500

    add help text to mex function

---
+++ +bfw/+mex/find_nearest_sorted.m
@@ -0,0 +1,29 @@
+function varargout = find_nearest_sorted(varargin)
+
+%   FIND_NEAREST_SORTED -- Find indices of elements of A closest to B, when
+%     both A and B are sorted arrays.
+%
+%     I = ... find_nearest_sorted( A, B ) for column vectors A and B returns 
+%     an array the same size as B containing indices of the elements of A
+%     closest to each B. That is, the i-th element in I gives the index
+%     into A such that abs(A(I(i)) - B(i)) is as small as possible for the
+%     given A. A and B must be sorted in ascending order. I is of class
+%     int64.
+%
+%     If A contains duplicate elements for which there could be multiple 
+%     absolute minima for a given element of B, the associated index is the 
+%     index of the last such element in A. More concretely,
+%     bfw.mex.find_nearest_sorted( [1, 1, 1, 1], 1 ) returns 4.
+%
+%     NaN elements in B are assigned the index 0 in I. NaN elements in A
+%     are skipped. If all elements of A are NaN, all indices in I are 1.
+%
+%     EX //
+%
+%     I = bfw.mex.find_nearest_sorted( 1:5, [1.1, 2.2, 2.8, 3.2] )
+%
+%     See also bfw.make.help
+
+error( 'No find_nearest_sorted mex function exists for your platform: "%s".', computer );
+
+end
