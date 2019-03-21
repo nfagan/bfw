8d6336a02c19a8cd9a5ce5da3b629fcceba51da0
nfagan <nick@fagan.org>
2019-03-21 15:01:18 -0400

    update build tool

--- +bfw/+mex/build_single_file.m
+++ +bfw/+mex/build_single_file.m
@@ -1,4 +1,4 @@
-function build_single_file(cpp_filename, increment_version)
+function build_single_file(cpp_filename, increment_version, varargin)
 
 %   BUILD_SINGLE_FILE -- Build and install mex file from single .cpp source file.
 %
@@ -11,7 +11,13 @@ function build_single_file(cpp_filename, increment_version)
 %
 %     See also mex
 
-if ( nargin < 2 )
+defaults = struct();
+defaults.gcc_path = '/usr/bin/gcc-4.9';
+defaults.gpp_path = '/usr/bin/g++-4.9';
+
+params = parsestruct( defaults, varargin );
+
+if ( nargin < 2 || isempty(increment_version) )
   increment_version = false;
 end
 
@@ -26,7 +32,7 @@ vers_filename_sans_ext = sprintf( '%s_version', filename_sans_ext );
 ver_src_file = fullfile( mex_dir, sprintf('%s.cpp', vers_filename_sans_ext) );
 
 if ( isunix() && ~ismac() )
-  compiler_spec = 'GCC=''/usr/bin/gcc-4.9'' G++=''/usr/bin/g++-4.9'' ';
+  compiler_spec = sprintf( 'GCC="%s" G++="%s" ', params.gcc_path, params.gpp_path );
   addtl_cxx_flags = 'CXXFLAGS="-std=c++1y -fPIC"';
 else
   compiler_spec = '';
@@ -85,5 +91,103 @@ function write_file(file_path, file_contents)
 fid = fopen( file_path, 'w+' );
 fprintf( fid, file_contents );
 fclose( fid );
+
+end
+
+function params = parsestruct(params, args)
+
+%   PARSESTRUCT -- Assign variable inputs to struct.
+%
+%     params = ... parsestruct( S, ARGS ) where ARGS is a cell array of
+%     'NAME', VALUE pairs assigns to `VALUE1` to field `NAME1` of struct 
+%     `S`, and so on for any additional number of ('name', value) pair 
+%     inputs. Each 'name' must be a present fieldname of `S`.
+%
+%     params = ... parsestruct( S, ARGS ) where ARGS is a cell array of 
+%     structs `S1`, `S2`, ... first decomposes those structs into a series 
+%     of 'field', value pairs, and assigns the contents to `S` as above.
+%
+%     params = ... parsestruct( S, ARGS ), where `ARGS` is a cell array of
+%     cell arrays `C1`, `C2`, ... recursively flattens those arrays into a 
+%     single series of 'field', value pairs, and assigns the contents to 
+%     `S` as above.
+%
+%     Note that `ARGS` can contain any valid combination of struct, cell,
+%     or 'name', value paired inputs.
+%
+%     EX //
+%
+%     s = struct( 'hello', 10, 'hi', 11 );
+%
+%     s1 = shared_utils.general.parsestruct( s, {'hello', 11} );
+%     s2 = shared_utils.general.parsestruct( s, {struct('hello', 11)} );
+%     s3 = shared_utils.general.parsestruct( s, {{'hello', 11}, struct('hi', 11)} );
+%
+%     IN:
+%       - `params` (struct)
+%       - `args` (cell)
+%     OUT:
+%       - `params` (struct)
+
+validateattributes( params, {'struct'}, {'scalar'}, 'parsestruct', 'params' );
+validateattributes( args, {'cell'}, {}, 'parsestruct', 'args' );
+
+try 
+  args = merge_struct_cell( args );
+catch err
+  throw( err );
+end
+
+N = numel( args );
+
+for i = 1:2:N
+  name = args{i};
+  
+  if ( isfield(params, name) )
+    params.(name) = args{i+1};
+  else
+    error( get_error_str_unrecognized_param(fieldnames(params), name) );
+  end
+end
+
+end
+
+function str = get_error_str_unrecognized_param(fields, name)
+
+base_text = sprintf( '"%s" is not a recognized parameter name.', name );
+field_text = sprintf( ' Options are:\n\n - %s', strjoin(sort(fields), '\n - ') );
+str = sprintf( '%s%s', base_text, field_text );
+
+end
+
+function s = merge_struct_cell(args)
+
+N = numel( args );
+stp = 1;
+
+s = {};
+
+while ( stp <= N )
+  v = args{stp};
+  
+  if ( ischar(v) )
+    assert( stp + 1 <= N, '"name", value pairs are incomplete.' );
+    
+    s = [ s, args(stp), args(stp+1) ];
+    stp = stp + 2;
+    
+  elseif ( iscell(v) )
+    s = [ s, merge_struct_cell(v) ];
+    stp = stp + 1;
+    
+  else
+    assert( isstruct(args{stp}), ['Inputs must come in "name", value pairs' ...
+      , ', or else be struct.'] );
+    
+    s = [ s, shared_utils.general.struct2varargin(v) ];
+    stp = stp + 1;
+  end
+end
+
 
 end
--- script/bfw_exclusive_events.m
+++ script/bfw_exclusive_events.m
@@ -75,37 +75,3 @@ end
 keep = find( keep );
 
 end
-
-function keep = slow_method(start_indices, stop_indices, labels, pairs, I)
-
-keep = true( numel(start_indices), 1 );
-
-for i = 1:numel(I)  
-  shared_utils.general.progress( i, numel(I) );
-  
-  for j = 1:numel(pairs)
-    is_a = find( labels, pairs{j}{1}, I{i} );
-    is_b = find( labels, pairs{j}{2}, I{i} );
-    
-    range_a = arrayfun( @(x, y) x:y, start_indices(is_a), stop_indices(is_a), 'un', 0 );
-    
-    for k = 1:numel(is_b)
-      c_is_b = is_b(k);
-      
-      start_b = start_indices(c_is_b);
-      stop_b = stop_indices(c_is_b);
-      
-      range_b = start_b:stop_b;
-      overlaps_with_a = cellfun( @(x) ~isempty(intersect(x, range_b)), range_a );
-      
-      if ( any(overlaps_with_a) )
-        keep(c_is_b) = false;
-      end
-    end
-  end
-end
-
-keep = find( keep );
-
-
-end
