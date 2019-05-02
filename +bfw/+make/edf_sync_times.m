7f074ae2fcfc0413a9bf27aca58f90fc73247de4
nfagan <nick@fagan.org>
2019-05-02 17:03:57 -0400

    update sync times

--- +bfw/+make/edf_sync_times.m
+++ +bfw/+make/edf_sync_times.m
@@ -18,6 +18,12 @@ unified_file = shared_utils.general.get( files, 'unified' );
 edf_file = shared_utils.general.get( files, 'edf' );
 
 unified_filename = bfw.try_get_unified_filename( unified_file );
+is_image_task = false;
+
+try
+  % Otherwise, not image task.
+  is_image_task = bfw.is_image_task( unified_file.m1.task_type );
+end
   
 fs = intersect( fieldnames(edf_file), {'m1', 'm2'} );
 
@@ -27,7 +33,7 @@ for j = 1:numel(fs)
   edf = edf_file.(fs{j}).edf;
   sync_times = unified_file.(fs{j}).plex_sync_times;
 
-  [fixed_times, start_time] = get_edf_sync_times( edf, sync_times );
+  [fixed_times, start_time] = get_edf_sync_times( edf, sync_times, is_image_task );
 
   sync_times_file.(fs{j}).unified_filename = unified_filename;
   sync_times_file.(fs{j}).edf_sync_times = fixed_times;
@@ -36,7 +42,7 @@ end
 
 end
 
-function [fixed_times, start_time] = get_edf_sync_times(edf, sync_times)
+function [fixed_times, start_time] = get_edf_sync_times(edf, sync_times, is_image_task)
 
 t = edf.Events.Messages.time;
 info = edf.Events.Messages.info;
@@ -47,6 +53,17 @@ is_resync_msg = strcmp( info, 'RESYNCH' );
 assert( sum(is_sync_msg) == 1, 'No starting synch message was found.' );
 assert( sum(is_resync_msg) == numel(sync_times)-1 ...
   , 'Number of resync times does not match given number of mat sync times.' );
+
+if ( is_image_task )
+  % Remove first sync time from image task.
+  first_resync = find( is_resync_msg, 1 );
+  first_sync = find( is_sync_msg, 1 );
+  
+  assert( first_resync < first_sync ...
+    , ' Expected first sync message to precede start sync message for image task.' );
+  
+  is_resync_msg(first_resync) = false;
+end
 
 fixed_times = t(is_resync_msg);
 start_time = t(is_sync_msg);
