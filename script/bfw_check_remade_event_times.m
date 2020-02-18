e5f6e8d3205ec70716a8556a7db5179fd313b103
nfagan <nick@fagan.org>
2020-02-18 16:46:49 -0500

    add event gatherer

---
+++ script/bfw_check_remade_event_times.m
@@ -0,0 +1,145 @@
+bounds_file_subdir = 'raw_events_remade_021820_bounds_file_method';
+roi_file_subdir = 'raw_events_remade_021820_roi_file_method';
+old_method_subdir = 'archive_021820';
+
+inputs = struct();
+
+bounds_file_event_outs = bfw_gather_events( 'event_subdir', bounds_file_subdir, inputs );
+roi_file_event_outs = bfw_gather_events( 'event_subdir', roi_file_subdir, inputs );
+old_method_event_outs = bfw_gather_events( 'event_subdir', old_method_subdir, inputs );
+
+%%
+
+stim_event_outs = bfw_gather_events( 'config', bfw_st.default_config() );
+
+%%
+
+use_event_file = old_method_event_outs;
+
+roi_labels = use_event_file.labels';
+mask = fcat.mask( roi_labels ...
+  , @find, 'm1' ...
+  , @find, {'eyes_nf', 'face'} ...
+);
+
+check_I = findall( roi_labels, {'unified_filename', 'roi'}, mask );
+start_indices = bfw.event_column( use_event_file, 'start_index' );
+stop_indices = bfw.event_column( use_event_file, 'stop_index' );
+
+num_use = numel( check_I );
+
+for i = 1:num_use
+  assert( numel(unique(start_indices(check_I{i}))) == numel(check_I{i}) );
+  
+  keep = bfw_exclusive_events( start_indices, stop_indices, roi_labels' ...
+    , {{'eyes_nf', 'face'}}, check_I(i) );
+  assert( numel(intersect(keep, check_I{i})) == numel(check_I{i}) );
+end
+
+%%
+
+bounds_events = bounds_file_event_outs.events;
+bounds_labels = bounds_file_event_outs.labels';
+addsetcat( bounds_labels, 'event_method', 'bounds_file' );
+
+roi_events = roi_file_event_outs.events;
+roi_labels = roi_file_event_outs.labels';
+addsetcat( roi_labels, 'event_method', 'roi_file' );
+
+old_events = old_method_event_outs.events;
+old_labels = old_method_event_outs.labels';
+addsetcat( old_labels, 'event_method', 'old_method' );
+
+event_key = bounds_file_event_outs.event_key;
+
+events = [ bounds_events; roi_events; old_events ];
+labels = [ bounds_labels'; roi_labels; old_labels ];
+
+bfw.add_monk_labels( labels );
+
+%%
+
+durations = events(:, event_key('duration'));
+
+thresholds = [0.025, 0.05, 0.075, 0.1];
+thresh_labels = arrayfun( @(x) sprintf('threshold_%s', num2str(x)), thresholds, 'un', 0 );
+
+[prop_labels, each_I] = keepeach( labels' ...
+  , {'unified_filename', 'roi', 'looks_by', 'event_method'} );
+
+below_thresh_labels = fcat();
+prop_below_thresh = [];
+
+for i = 1:numel(each_I)
+  ps = nan( size(thresholds) );
+  
+  for j = 1:numel(thresholds)
+    ps(j) = pnz( durations(each_I{i}) < thresholds(j) );
+  end
+  
+  prop_below_thresh = [ prop_below_thresh; ps(:) ];
+  
+  tmp_labels = append1( fcat(), prop_labels, i, numel(thresholds) );
+  addsetcat( tmp_labels, 'threshold', thresh_labels );
+  append( below_thresh_labels, tmp_labels );
+end
+
+assert_ispair( prop_below_thresh, below_thresh_labels );
+
+%%
+
+pl = plotlabeled.make_common();
+pl.y_lims = [0, 1];
+
+fcats = { 'event_method' };
+xcats = { 'looks_by' };
+gcats = { 'threshold' };
+pcats = { 'roi', 'event_method' };
+
+mask = fcat.mask( below_thresh_labels ...
+  , @find, {'roi_file', 'old_method'} ...
+  , @find, combs(below_thresh_labels, 'roi', find(below_thresh_labels, 'roi_file')) ...
+);
+
+fig_I = findall_or_one( below_thresh_labels, fcats, mask );
+
+for i = 1:numel(fig_I)
+  figure(i);  
+  tmp_data = prop_below_thresh(fig_I{i});
+  tmp_labels = prune( below_thresh_labels(fig_I{i}) );
+  
+  axs = pl.bar( tmp_data, tmp_labels, xcats, gcats, pcats );
+end
+
+%%
+
+% mask = find( durations >= 0.07 );
+mask = rowmask( labels );
+
+[count_labels, each_I] = keepeach( labels' ...
+  , {'unified_filename', 'roi', 'looks_by', 'event_method'}, mask );
+counts = cellfun( @numel, each_I );
+
+pl = plotlabeled.make_common();
+pl.summary_func = @nanmedian;
+% pl.y_lims = [0, 1];
+
+fcats = { };
+xcats = { 'looks_by' };
+gcats = { 'roi' };
+pcats = { 'event_method', 'id_m1' };
+
+mask = fcat.mask( count_labels ...
+  , @find, {'roi_file', 'old_method'} ...
+  , @find, {'eyes_nf', 'face'} ...
+);
+
+fig_I = findall_or_one( count_labels, fcats, mask );
+
+for i = 1:numel(fig_I)
+  figure(i);  
+  tmp_data = counts(fig_I{i});
+  tmp_labels = prune( count_labels(fig_I{i}) );
+  
+  axs = pl.bar( tmp_data, tmp_labels, xcats, gcats, pcats );
+end
---
+++ script/bfw_gather_events.m
@@ -0,0 +1,57 @@
+function outs = bfw_gather_events(varargin)
+
+defaults = bfw.get_common_make_defaults();
+defaults.event_subdir = '';
+
+params = bfw.parsestruct( defaults, varargin );
+
+inputs = { fullfile('raw_events', params.event_subdir), 'meta', 'stim_meta' };
+
+[~, runner] = bfw.get_params_and_loop_runner( inputs, '', defaults, varargin );
+runner.convert_to_non_saving_with_output();
+
+results = runner.run( @gather, params );
+outputs = shared_utils.pipeline.extract_outputs_from_results( results );
+
+outs = struct();
+
+if ( isempty(outputs) )
+  outs.events = [];
+  outs.event_key = containers.Map();
+  outs.labels = fcat();
+else
+  outs.events = vertcat( outputs.events );
+  outs.event_key = outputs(1).event_key;
+  outs.labels = vertcat( fcat, outputs.labels );
+end
+
+end
+
+function out = gather(files, params)
+
+import shared_utils.general.*;
+
+if ( isempty(params.event_subdir) )
+  event_subdir = 'raw_events';
+else
+  event_subdir = params.event_subdir;
+end
+
+events_file = get( files, event_subdir );
+meta_file = get( files, 'meta' );
+stim_meta_file = get( files, 'stim_meta' );
+
+events = events_file.events;
+event_key = events_file.event_key;
+
+labels = join( fcat.from(events_file) ...
+  , bfw.struct2fcat(meta_file) ...
+  , bfw.stim_meta_to_fcat(stim_meta_file) ...
+);
+
+out = struct();
+out.events = events;
+out.event_key = event_key;
+out.labels = labels;
+
+end
