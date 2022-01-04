19068eb08b4599155466c88ba36c4d9a00d5040b
nfagan <nick@fagan.org>
2022-01-04 14:42:54 -0500

    accept mutual events with mouth

--- +bfw/make_aligned_samples.m
+++ +bfw/make_aligned_samples.m
@@ -6,6 +6,7 @@ params = bfw.parsestruct( defaults, varargin );
 bfw.make_aligned_time( params );
 bfw.make_aligned_position( params );
 bfw.make_aligned_bounds( params );
+bfw.make_aligned_pupil_size( params );
 bfw.make_aligned_fixations( params, 'fixations_subdir', 'raw_eye_mmv_fixations' );
 bfw.make_aligned_fixations( params, 'fixations_subdir', 'raw_arduino_fixations' );
 
