c58229690459042d5880b56b020ed302ff771324
nfagan <nick@fagan.org>
2022-05-09 13:51:42 -0400

    add mouth for stim

---
+++ +bfw/default_stim_task_roi_ordering_mouth.m
@@ -0,0 +1,6 @@
+function [excl_roi_order, mut_roi_order] = default_stim_task_roi_ordering_mouth(roi_file)
+
+excl_roi_order = { 'eyes_nf', 'mouth', 'face' };
+mut_roi_order = { 'eyes_nf', 'mouth', 'face' };
+
+end
